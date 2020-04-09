c8cd548167a58219fcbd7d71d533b0efd96df4b6
YvetteMiao <mywyvette220@gamil.com>
2020-04-09 21:07:32 +0200

    mise en place des tests

--- src/main/groovy/gl51/MovieController.groovy
+++ src/main/groovy/gl51/MovieController.groovy
@@ -1,15 +1,23 @@
 package gl51
 
+import gl51.movie.data.Movie
+import gl51.movie.data.MovieRequest
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.Post
 
 
 @Controller("/movie")
 class MovieController {
 
     @Get("/")
-    HttpStatus index() {
-        return HttpStatus.OK
+    List<Movie> index() {
+        []
+    }
+
+    @Post('/')
+    HttpStatus addMovie(MovieRequest movieRequest) {
+        HttpStatus.CREATED
     }
 }
